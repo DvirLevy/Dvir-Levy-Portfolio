487eb0357aa0f2cd3141d8b07022b4409251abc1
Dvir Levy <66549662+DvirLevy@users.noreply.github.com>
2026-04-11 11:28:21 +0300

    Update path to dvir.png

--- src/components/PortfolioBotWidget.tsx
+++ src/components/PortfolioBotWidget.tsx
@@ -8,7 +8,7 @@ import {
 import { Button } from "@/components/ui/button"
 import { Mic, X, MessageCircleQuestion } from "lucide-react"
 import { useBotService } from "@/botService"
-import dvirImage from "../assets/dvir.png"
+import dvirImage from "../../public/dvir.png"
 
 export const PortfolioBotWidget = () => {
   const [isOpen, setIsOpen] = useState(false)
